7b6e04838d8277eb5b061ea683cf3f82ce195002
daiyihui <616574460@qq.com>
2022-02-24 23:44:07 +0800

    移除IDE帮助包

--- config/app.php
+++ config/app.php
@@ -176,8 +176,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-
 
     ],
 
